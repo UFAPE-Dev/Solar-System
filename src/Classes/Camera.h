09ed50baee9e2d398ffcc7edcce9a36a13a648f2
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2022-05-26 21:51:31 -0300

    Ajustes de câmera

--- src/Classes/Camera.h
+++ src/Classes/Camera.h
@@ -16,8 +16,8 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
+const float YAW         = -195.1f;
+const float PITCH       =  -22.9F;
 const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
 const float ZOOM        =  45.0f;
